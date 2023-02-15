22c54ac1269f5a0125fb3d998d2ddca85beb4bb2
1807869 <yysun2000@thehyundai.com>
2023-02-15 10:52:46 +0900

    기존 css들 모듈러로 처리하려고 시도함.

--- src/components/common/homebutton/index.js
+++ src/components/common/homebutton/index.js
@@ -1,16 +1,19 @@
 import "./homebutton.css";
-import {Link} from 'react-router-dom'; 
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const HomeButton = () => {
-    const location = useLocation();
-    const routerlist = useSelector(state => state.routerlist)
-    const [sourceURL,setSourceURL] = useState("");
-    return (<div className="HomeButton">
-        <Link to="/">목록으로 돌아가기</Link><br/>
-    </div>)
-}
+  const location = useLocation();
+  const routerlist = useSelector((state) => state.routerlist);
+  const [sourceURL, setSourceURL] = useState("");
+  return (
+    <div className="HyundaiDEPTHomeButton">
+      <Link to="/">목록으로 돌아가기</Link>
+      <br />
+    </div>
+  );
+};
 
 export default HomeButton;
